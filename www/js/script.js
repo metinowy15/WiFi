5ccb59e39ad692dc964e91cc1d10a40c4356d0f0
Jakub koza <Jakub koza>
2017-03-13 18:20:48 +0100

    dsds

---
+++ www/js/script.js
@@ -0,0 +1,42 @@
+window.addEventListener("DOMContentLoaded", start);
+var Pojemnik;
+
+function start() {
+    Pojemnik = document.getElementById("polaczenie");
+    Pojemnik.innerHTML += "Test1";
+    init();
+
+}
+
+function init() {
+
+
+    document.addEventListener("deviceready", gotowe, false);
+}
+
+
+function gotowe() {
+
+
+    Pojemnik.innerHTML = zwrocPolaczenie();
+    Pojemnik.innerHTML += "  Test2";
+}
+
+
+function zwrocPolaczenie() {
+
+
+    var twojePolaczenie = connection.type;
+
+    var polaczenia = {};
+
+    polaczenia[Connection.UNKNOWN] = "Polaczenie nieznane";
+    polaczenia[Connection.ETHERNET] = "Polaczenie kablem ethernet";
+    polaczenia[Connection.CELL_3G] = "Masz połączenie 3G!";
+    polaczenia[Connection.CELL_4G] = "Brawo! Masz połączenie 4G";
+    polaczenia[Connection.WIFI] = "Łączysz sie przez wifi";
+    polaczenia[Connection.NONE] = "Nie masz połączenia";
+
+    return polaczenia[twojePolaczenie];
+
+}
